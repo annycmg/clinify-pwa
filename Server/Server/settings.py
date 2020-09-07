99782a9eac67ac70e2e1aea1ab7706961e230a77
anny_ <anny_cmgoncalves@hotmail.com>
2020-09-06 23:42:15 -0300

    PWA: manifest.json

--- Server/Server/settings.py
+++ Server/Server/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'server_app',
-]
+    'server_app']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
