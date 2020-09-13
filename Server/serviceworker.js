d2826ec6e3e72bf88ce8797f30385eac3fc8bc48
anny_ <anny_cmgoncalves@hotmail.com>
2020-09-12 21:43:13 -0300

    PWA: with manifest and homescreen banner only

--- Server/serviceworker.js
+++ Server/serviceworker.js
@@ -9,28 +9,3 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', function(event) {
     console.log('[Service Worker] Fetching Service Worker ...', event);
 });
-
-if (window.Notification) {
-    function showNotification() {
-        let NotificationOpts = {
-            body: 'Some notification information',
-            icon: '/images/heartbeat-icon-home-144.png'
-        }
-        new Notification("My new notification");
-    }
-}
-
-if (Notification.permission === 'granted') {
-    showNotification();
-} else if (Notification.permission !== 'denied') {
-    Notification.requestPermission((permission) => {
-        if (permission === 'granted') {
-            showNotification();
-        }
-    });
-}
-
-self.addEventListener('push', event => {
-    let n = self.registration.showNotification('A notification from service worker...');
-    event.waitUntil(n);
-})
