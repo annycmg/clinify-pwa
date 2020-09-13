a5cead4f47f1a19629a156e74c02c69db3363d30
anny_ <anny_cmgoncalves@hotmail.com>
2020-09-12 21:59:29 -0300

    Packaging

--- Server/serviceworker.js
+++ Server/serviceworker.js
@@ -2,7 +2,7 @@ self.addEventListener('install', function (event) {
     console.log('[Service Worker] Installing Service Worker ...', event);
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate', function (event) {
     console.log('[Service Worker] Activating Service Worker ...', event);
 });
 
